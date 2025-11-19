ad823f01dcfb31eac2ad2f2be3c11872fec03936
Leandro <le_andro18@hotmail.com>
2025-11-18 22:27:08 -0300

    feat: Implement run API

---
+++ run/network/src/main/java/com/leandrour/run/network/RunMappers.kt
@@ -0,0 +1,34 @@
+package com.leandrour.run.network
+
+import com.leandrour.core.domain.location.Location
+import com.leandrour.core.domain.run.Run
+import java.time.Instant
+import java.time.ZoneId
+import kotlin.time.Duration.Companion.milliseconds
+
+fun RunDto.toRun(): Run {
+    return Run(
+        id = id,
+        duration = durationMillis.milliseconds,
+        dateTimeUtc = Instant.parse(dateTimeUtc).atZone(ZoneId.of("UTC")),
+        distanceMeters = distanceMeters,
+        location = Location(lat, long),
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}
+
+fun Run.toCreateRunRequest(): CreateRunRequest {
+    return CreateRunRequest(
+        id = id!!,
+        durationMillis = duration.inWholeMilliseconds,
+        distanceMeters = distanceMeters,
+        epochMillis = dateTimeUtc.toEpochSecond() * 1000L,
+        lat = location.latitude,
+        long = location.longitude,
+        avgSpeedKmh = avgSpeedKmh,
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters
+    )
+}
