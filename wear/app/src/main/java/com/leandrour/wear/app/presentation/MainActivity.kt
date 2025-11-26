bac86126875bed350899e916e1e7a395eb41aa8c
Leandro <le_andro18@hotmail.com>
2025-11-25 22:12:01 -0300

    feat: Add Wear OS theme setup

--- wear/app/src/main/java/com/leandrour/wear/app/presentation/MainActivity.kt
+++ wear/app/src/main/java/com/leandrour/wear/app/presentation/MainActivity.kt
@@ -3,7 +3,14 @@ package com.leandrour.wear.app.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material3.Button
+import androidx.wear.compose.material3.Text
+import com.leandrour.core.presentation.designsystem_wear.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +21,16 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-
+            RuniqueTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Hello world")
+                    }
+                }
+            }
         }
     }
 }
